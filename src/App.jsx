ebd67a78bc139e6529e4654ddb16bd0781fa8683
iamkoushik1999 <iamkoushik1999@gmail.com>
2025-02-07 23:17:57 +0530

    Modify: app.js

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ import { Toaster } from 'react-hot-toast';
 // Pages
 import Restaurant from './pages/Restaurants/Restaurant';
 import RestaurantForm from './pages/Restaurants/RestaurantForm';
+import Home from './pages/Home/Home';
+import AboutUsPage from './pages/About/AboutUsPage';
+import PrivacyPolicyPage from './pages/PrivacyPolicy/PrivacyPolicyPage';
+import TermsPage from './pages/Terms/TermsPage';
+import ContactUsPage from './pages/ContactUs/ContactUsPage';
+import NotFoundPage from './pages/NotFound/NotFoundPage';
 
 const App = () => {
   return (
@@ -29,10 +35,17 @@ const App = () => {
 
         {/* Application Routes */}
         <Routes>
-          <Route path='/' element={<Restaurant />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<AboutUsPage />} />
+          <Route path='/privacy-policy' element={<PrivacyPolicyPage />} />
+          <Route path='/terms-conditions' element={<TermsPage />} />
+          <Route path='/contact-us' element={<ContactUsPage />} />
+          {/* <Route path='/not-found' element={<NotFoundPage />} /> */}
+          <Route path='/restaurant' element={<Restaurant />} />
           <Route path='/add' element={<RestaurantForm />} />
           <Route path='/edit/:id' element={<RestaurantForm />} />
-          <Route path='*' element={<Navigate to='/' replace />} />
+          {/* <Route path='*' element={<Navigate to='/' replace />} /> */}
+          <Route path='*' element={<NotFoundPage />} />
         </Routes>
       </div>
     </Router>
