d91e702737f26b48404fc393c6aeb7a6ca903c89
iamkoushik1999 <iamkoushik1999@gmail.com>
2025-02-08 23:21:24 +0530

    Add: category

---
+++ src/pages/Category/CategoryFormPage.jsx
@@ -0,0 +1,11 @@
+import CategoryForm from '../../components/Category/CategoryForm/CategoryForm';
+
+const CategoryFormPage = () => {
+  return (
+    <div>
+      <CategoryForm />
+    </div>
+  );
+};
+
+export default CategoryFormPage;
---
+++ src/pages/Category/CategoryListPage.jsx
@@ -0,0 +1,11 @@
+import CategoryList from '../../components/Category/CategoryList/CategoryList';
+
+const CategoryListPage = () => {
+  return (
+    <div>
+      <CategoryList />
+    </div>
+  );
+};
+
+export default CategoryListPage;
