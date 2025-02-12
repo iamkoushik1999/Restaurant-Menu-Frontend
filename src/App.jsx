dd3e43d308525c81a6bc94604d119e86f7aea309
iamkoushik1999 <iamkoushik1999@gmail.com>
2025-02-12 23:29:48 +0530

    Fix: Splash Screen for only home

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,10 @@
-// import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// // React Hot Toast
-// import { Toaster } from 'react-hot-toast';
-// // Pages
-// import RestaurantListPage from './pages/Restaurants/RestaurantListPage';
-// import RestaurantFormPage from './pages/Restaurants/RestaurantFormPage';
-// import Home from './pages/Home/Home';
-// import AboutUsPage from './pages/About/AboutUsPage';
-// import PrivacyPolicyPage from './pages/PrivacyPolicy/PrivacyPolicyPage';
-// import TermsPage from './pages/Terms/TermsPage';
-// import ContactUsPage from './pages/ContactUs/ContactUsPage';
-// import NotFoundPage from './pages/NotFound/NotFoundPage';
-// import CategoryFormPage from './pages/Category/CategoryFormPage';
-// import CategoryListPage from './pages/Category/CategoryListPage';
-
-// // Implement Lazy Loading
-// // Implement Start Page Flash
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div>
-//         {/* React Hot Toast Toaster */}
-//         <Toaster
-//           position='top-center'
-//           duration={3000}
-//           toastOptions={{
-//             style: {
-//               background: '#333',
-//               color: '#fff',
-//             },
-//           }}
-//         />
-
-//         {/* Application Routes */}
-//         <Routes>
-//           <Route path='/' element={<Home />} />
-//           <Route path='/about' element={<AboutUsPage />} />
-//           <Route path='/privacy-policy' element={<PrivacyPolicyPage />} />
-//           <Route path='/terms-conditions' element={<TermsPage />} />
-//           <Route path='/contact-us' element={<ContactUsPage />} />
-//           {/* Restaurant */}
-//           <Route path='/restaurant' element={<RestaurantListPage />} />
-//           <Route path='/restaurant/add' element={<RestaurantFormPage />} />
-//           <Route path='/restaurant/edit/:id' element={<RestaurantFormPage />} />
-//           {/* Category */}
-//           <Route path='/add-category/:id' element={<CategoryFormPage />} />
-//           <Route path='/categories/:id' element={<CategoryListPage />} />
-//           {/* 404 */}
-//           <Route path='*' element={<NotFoundPage />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { lazy, Suspense, useState, useEffect } from 'react';
+import { lazy, Suspense } from 'react';
 // React Hot Toast
 import { Toaster } from 'react-hot-toast';
-import { Box } from '@mui/material';
-import { DotLoader } from 'react-spinners';
+// Components
+import Loader from './components/Loader/CustomLoader';
 
 // Lazy Loading Pages
 const Home = lazy(() => import('./pages/Home/Home'));
@@ -91,35 +32,8 @@ const CategoryListPage = lazy(() =>
   import('./pages/Category/CategoryListPage')
 );
 
-// Splash Screen Component
-const SplashScreen = () => (
-  <Box display='flex' justifyContent='center' alignItems='center' height='90vh'>
-    <DotLoader
-      color='#1976d2'
-      margin={2}
-      radius={20}
-      speedMultiplier={1}
-      aria-label='loading'
-      data-testid='loader'
-      size={100}
-    />
-  </Box>
-);
-
 const App = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate a loading delay (2 seconds)
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  return loading ? (
-    <SplashScreen />
-  ) : (
+  return (
     <Router>
       <div>
         {/* React Hot Toast Toaster */}
@@ -135,7 +49,7 @@ const App = () => {
         />
 
         {/* Application Routes */}
-        <Suspense fallback={<SplashScreen />}>
+        <Suspense fallback={<Loader />}>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<AboutUsPage />} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
 import LandingPage from '../../components/Home/LandingPage';
+import SplashScreen from '../../components/Loader/SplashScreen';
 
 const Home = () => {
-  return (
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay (2 seconds)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return loading ? (
+    <SplashScreen />
+  ) : (
     <div>
       <LandingPage />
     </div>
